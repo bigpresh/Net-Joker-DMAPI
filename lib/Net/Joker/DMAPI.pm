5207de181630d10b73a0acd01ebbe17722dca943
David Precious <davidp@preshweb.co.uk>
2014-10-08 00:27:45 +0100

    Document attributes & methods properly

--- lib/Net/Joker/DMAPI.pm
+++ lib/Net/Joker/DMAPI.pm
@@ -27,6 +27,13 @@ much as possible.
 
     my $whois_details = $dmapi->query_whois($domain);
 
+=head1 ATTRIBUTES
+
+=over
+
+=item username
+
+Your Joker account username.
 
 =cut
 
@@ -34,30 +41,62 @@ has username => (
     is => 'rw',
     isa => 'Str',
 );
+
+=item password
+
+Your Joker account password
+
+=cut
+
 has password => (
     is => 'rw',
     isa => 'Str',
 );
+
+=item debug
+
+Whether to omit debug messages; disabled by default, set to a true value to
+enable.
+
+=cut
+
 has debug => (
     is => 'rw',
     isa => 'Str',
     default => 0,
 );
+
+=item ua
+
+An LWP::UserAgent object to use.  One is constructed by default, so you don't
+need to supply this unless you have a specific need to do so.
+
+=cut
+
 has ua => (
     is => 'rw',
     isa => 'LWP::UserAgent',
     lazy_build => 1,
-);
-has dmapi_url => (
-    is => 'rw',
-    isa => 'Str',
-    default => 'https://dmapi.joker.com/request',
 );
 sub _build_ua {
     my $ua = LWP::UserAgent->new;
     $ua->agent(__PACKAGE__ . "/$VERSION");
     return $ua;
 }
+
+=item dmapi_url
+
+The URL to Joker's DMAPI.  You won't need to provide this unless you for some
+reason need to have requests go elsewhere; it defaults to Joker's live DMAPI
+URL.
+
+=cut
+
+has dmapi_url => (
+    is => 'rw',
+    isa => 'Str',
+    default => 'https://dmapi.joker.com/request',
+);
 
 has auth_sid => (
     is => 'rw',
@@ -66,12 +105,30 @@ has auth_sid => (
     predicate => 'has_auth_sid',
 );
 
+=back
+
+=head1 METHODS
+
+=over
+
+=item login
+
+Logs in to the Joker DMAPI, retrieves the C<Auth-Sid> from the response, and
+stores it in the C<auth_sid> attribute for future requests.  You won't usually
+need to call this, as it will happen automatically if you use the convenience
+methods, but if you want to poke at C<do_request> yourself, you'll need it.
+
+=cut
+
 sub login {
     my $self = shift;
     
     # If we've already logged in, we're fine
     # TODO: do we need to test the auth-sid is still valid?
-    return 1 if $self->has_auth_sid;
+    if (!$self->has_auth_sid) {
+        $self->debug_output("Already have auth_sid, no need to log in");
+        return 1;
+    }
 
     my $login_result = $self->do_request(
         'login',
@@ -87,10 +144,30 @@ sub login {
     }
 }
 
+
+=item do_request
+
+Takes the method name you want to call, and a hashref of arguments, calls the
+method, and returns the response.
+
+For instance:
+
+  my $response = $dmapi->do_request('query-whois', { domain => $domain });
+
+The response returned is as given by Joker's (inconsistent) API, though; so
+you'll probably want to look for a suitable method in this class which takes
+care of parsing the response and returning something useful.  If a method for
+the DMAPI method you wish to use doesn't yet exist, contact me or submit a patch
+:)  In particular, some requests don't return the result, just an ID which
+you'll then need to use to poll for the result.
+
+=cut
+
 # Given a method name and some params, perform the request, check for success,
 # and return the result
 sub do_request {
     my ($self, $method, $params) = @_;
+
     my $url = $self->form_request_url($method, $params);
     $self->debug_output("Calling $method - URL: $url");
     my $response = $self->ua->get($url);
